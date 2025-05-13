4eefc48434c6d21f2fb4c82ada32548c4bd80a3f
Justin Mottier <j.mottier@niryo.com>
2025-05-13 09:44:55 +0200

    feat: implement remaining endpoints

--- pyniryo/nate/components/users.py
+++ pyniryo/nate/components/users.py
@@ -17,3 +17,15 @@ class Users(BaseAPIComponent):
 
     def delete(self, user_id: str) -> None:
         return self._http_client.delete(paths_gen.Users.USER.format(user_id=user_id), None, None)
+
+    def update(self, user: models.User) -> models.User:
+        return self._http_client.patch(paths_gen.Users.USER.format(user_id=user.id), user, models.User)
+
+    def get_tokens(self, user_id: str) -> list[models.Token]:
+        return self._http_client.get(paths_gen.Users.USER_TOKENS.format(user_id=user_id), models.TokenList).root
+
+    def create_token(self, user_id: str, token: models.TokenCreation) -> models.Token:
+        return self._http_client.post(paths_gen.Users.USER_TOKENS.format(user_id=user_id), token, models.Token)
+
+    def update_password(self, user_id: str, update_password: models.UpdatePassword) -> None:
+        return self._http_client.patch(paths_gen.Users.USER_PASSWORD.format(user_id=user_id), update_password, None)
