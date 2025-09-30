fa9c9c902c6d508a64c22ffca2347facbc0127af
Justin Mottier <j.mottier@niryo.com>
2025-09-30 17:42:09 +0200

    doc: add execute_program example file

---
+++ examples/nate/execute_program.py
@@ -0,0 +1,38 @@
+import logging
+from io import StringIO
+from pathlib import Path
+
+from pyniryo.nate.client import Nate
+from pyniryo.nate.models import ProgramType
+
+logging.basicConfig(
+    level=logging.INFO,  # or DEBUG
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+)
+
+
+def main():
+    n = Nate()
+
+    move_poses_path = Path(__file__).parent.joinpath("move_poses.py")
+    with open(move_poses_path, "r") as program_file:
+        program = n.programs.create(move_poses_path.stem, program_file, program_type=ProgramType.python())
+
+    program_output = StringIO()
+    execution = n.programs.execute(
+        program.id,
+        environment={
+            "NATE_INSECURE": "1",
+            "NATE_USERNAME": "admin",
+            "NATE_PASSWORD": "@dm1n",
+        },
+        stdout=program_output,
+    )
+    execution.wait()
+    print(f'Execution finished with exit code {execution.execution.exitCode}')
+    print('Program output:')
+    print(program_output.getvalue())
+
+
+if __name__ == "__main__":
+    main()
