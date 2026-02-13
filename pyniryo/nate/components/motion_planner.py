f3cc94028148035d1b7c826ed45d138cee830539
Justin Mottier <j.mottier@niryo.com>
2026-02-13 09:33:56 +0100

    feat: add_stop

--- pyniryo/nate/components/motion_planner.py
+++ pyniryo/nate/components/motion_planner.py
@@ -7,9 +7,9 @@ from . import BaseAPIComponent
 
 class MotionPlanner(BaseAPIComponent):
 
-    def generate_trajectory(self, waypoints: models.Waypoints) -> models.Trajectory:
+    def generate_trajectory(self, waypoints: models.Waypoints, add_start: bool = False) -> models.Trajectory:
         req = transport_models.TrajectoryGeneration(waypoints=[wp.to_transport_model() for wp in waypoints],
-                                                    motion_type=transport_models.MotionType.waypoints,
-                                                    add_start=None)
+                                                    motion_type=transport_models.MotionType.WAYPOINTS,
+                                                    add_start=add_start)
         resp = self._http_client.post(paths_gen.Robot.GENERATE_TRAJECTORY, transport_models.GeneratedTrajectory, req)
         return models.Trajectory.from_transport_model(resp.waypoints)
